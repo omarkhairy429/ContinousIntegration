2d408fb6639bfd9a677488384bda521c30bd5c7c
omarkhairy429 <omar.fayed.5832@gmail.com>
2026-07-26 02:51:52 +0300

    Initializing the project

---
+++ src/main/java/com/orange/Pipeline/PipelineApplication.java
@@ -0,0 +1,13 @@
+package com.orange.Pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineApplication.class, args);
+	}
+
+}
