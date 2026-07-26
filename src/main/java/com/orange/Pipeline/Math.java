b01c5ca574f5fcd3e237a0dc03f675b11493781d
omarkhairy429 <omar.fayed.5832@gmail.com>
2026-07-26 03:20:18 +0300

    Writing simple Math class, Testing it and writing the initial pipeline

---
+++ src/main/java/com/orange/Pipeline/Math.java
@@ -0,0 +1,7 @@
+package com.orange.Pipeline;
+
+public class Math {
+    public static double sum(double number1, double number2) {
+        return number1 + number2;
+    }
+}
--- src/test/java/com/orange/Pipeline/PipelineApplicationTests.java
+++ src/test/java/com/orange/Pipeline/PipelineApplicationTests.java
@@ -3,11 +3,21 @@ package com.orange.Pipeline;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class PipelineApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void MathSumTest() {
+		double num1 = 10;
+		double num2 = 20;
+		double sum = Math.sum(num1, num2);
+		assertEquals(30, sum);
 	}
 
 }
